f1006c31fa19595e26cf750f7ee6185db3b501e7
applele <noyeyeong3@gmail.com>
2025-04-30 15:53:32 +0900

    v0.1 hamming code

--- hamming_code.py
+++ hamming_code.py
@@ -12,16 +12,16 @@ def hamming_code(hcode):
         # 해당 패리티 비트가 검사하는 비트들의 합 계산
         for j in range(idx, n, 2*(idx+1)):
             count += sum(int(hcode[k]) for k in range(j, min(j+idx+1, n)))
-            if count % 2 != 0:
-                error_position += idx + 1
-            i = i + 1
+        if count % 2 != 0:
+            error_position += idx + 1
+        i = i + 1
         # 오류가 있으면 해당 비트 반전
-        if error_position != 0 and error_position <= n:
-            #hcode[error_position - 1] = '0' if hcode[error_position - 1] == '1' else '1'
-            if hcode[error_position - 1] == '1':
-                hcode[error_position - 1] = '0'
-            else:
-                hcode[error_position - 1] = '1'
+    if error_position != 0 and error_position <= n:
+        hcode[error_position - 1] = '0' if hcode[error_position - 1] == '1' else '1'
+            # if hcode[error_position - 1] == '1':
+            #     hcode[error_position - 1] = '0'
+            # else:
+            #     hcode[error_position - 1] = '1'
     return ''.join(hcode)
 
 input_code = input("Input hamming code(binary) : ")     #ex) 111101001010
